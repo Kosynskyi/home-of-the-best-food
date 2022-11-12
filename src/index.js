171b11d9a234f535c07ecadf5cb05fb94f64a3e1
Kosynskyi <kosynskyi.vova@gmail.com>
2022-11-12 19:23:58 +0200

    finish locale storage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
+
+const STORAGE_KEY = 'dataOrder';
 
 const refs = {
+  body: document.body,
   burgerMenu: document.querySelector('.header__menu-btn'),
   // isOpen: document.querySelector('.header__burger-open'),
   // isClose: document.querySelector('.header__burger-close'),
@@ -8,11 +12,14 @@ const refs = {
   orderTableBtn: document.querySelector('.button__primary--celebrate'),
   orderTableBtnHeader: document.querySelector('.button__primary--header'),
   orderTableBtnFooter: document.querySelector('.button__primary--footer'),
+  formDataOrder: document.querySelector('.order-table__form'),
   modalBackdrop: document.querySelector('.order-table__backdrop'),
   btnCloseOrderModal: document.querySelector('.order-table__btn-close'),
   btnViewMenu: document.querySelector('.button__primary--hero'),
   modalViewBackdrop: document.querySelector('.view-menu__backdrop'),
   btnViewMenuClose: document.querySelector('.view-menu__btn-close'),
+  // ==================view-menu===========================
+  viewMenuList: document.querySelector('.view-menu__list'),
 };
 
 refs.burgerMenu.addEventListener('click', onClick);
@@ -27,6 +34,8 @@ refs.modalViewBackdrop.addEventListener(
   onCloseModalViewMenuByBackdrop
 );
 refs.btnViewMenuClose.addEventListener('click', onCloseModalViewMenuByBtn);
+refs.formDataOrder.addEventListener('input', onSaveDataOrder);
+refs.formDataOrder.addEventListener('submit', onFormOrderSubmit);
 
 // функция открытия бургур меню
 function onClick() {
@@ -56,13 +65,49 @@ function onCloseBackdropModal(e) {
     refs.modalBackdrop.classList.remove('is-open');
   }
 }
+
+//функция сбора данных и сохранения для local storage
+function onSaveDataOrder(e) {
+  const dataOrder = {
+    name: e.currentTarget.elements.name.value,
+    surname: e.currentTarget.elements.surname.value,
+    date: e.currentTarget.elements.date.value,
+    time: e.currentTarget.elements.time.value,
+    comments: e.currentTarget.elements.comments.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataOrder));
+}
+
+//функция отправки данных и очистки формы
+function onFormOrderSubmit(e) {
+  e.preventDefault();
+
+  if (
+    e.currentTarget.elements.date.value === '' ||
+    e.currentTarget.elements.time.value === ''
+  ) {
+    return Notiflix.Notify.warning('Дата или время не может быть пустым');
+  }
+
+  console.log('нажали отправить форму');
+  refs.formDataOrder.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
 // ======================view menu modal=========================
 // функция открытия модалки view menu
 function onOpenModalViewMenu() {
-  console.log('Клик на onOpenModalViewMenu ');
   refs.modalViewBackdrop.classList.toggle('is-open');
-  // getRandomDish();
+  // fixedBody();
+  createAndRenderRandomMarkup();
 }
+
+// функция фиксирования body от прокрутки
+
+// function fixedBody() {
+//   refs.body.style.position = 'fixed';
+//   refs.body.style.top = `-${window.scrollY}px`;
+// }
 
 // функция закрытия модалки ПО БЕКДРОПУ view menu
 function onCloseModalViewMenuByBackdrop(e) {
@@ -80,61 +125,48 @@ function onCloseModalViewMenuByBtn() {
 
 const BASE_URL = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
-// const qwe = async () => {
-//   let arr = [];
-//   for (let i = 0; i < 2; i += 1) {
-//     const dish = await fetch(BASE_URL);
-//     console.log(dish);
-//     arr.push(dish);
-//     return dish;
-//   }
-// };
+async function getRandomDish() {
+  try {
+    let arr = [];
+    for (let i = 0; i < 10; i += 1) {
+      const dish = await axios(BASE_URL);
+      arr.push(dish);
+    }
 
-// console.log(qwe());
+    const promiseArr = await Promise.all(arr).then(response => {
+      return response;
+    });
+    return promiseArr;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
 
-// https://jsonplaceholder.typicode.com/users
+function createRandomMarkup(arr) {
+  return arr
+    .map(item => {
+      const { strMeal, strMealThumb } = item.data.meals[0];
+      return `<li class="view-menu view-menu__item">
+    <img class="view-menu view-menu__img" src="${strMealThumb}" width="80" alt="Рандомная картинка еды"/>
+  <p class="view-menu view-menu__name">${strMeal}</p>
+  </li>
+  
+  `;
+    })
+    .join('');
+}
 
-//
+function renderMarkup(element, markup) {
+  element.innerHTML = '';
+  element.insertAdjacentHTML('beforeend', markup);
+}
 
-// fetch()
-//   .then(meal => console.log(meal))
-//   .catch(error => console.log(error));
-
-// async function getRandomDish() {
-//   try {
-//     let arr = [];
-// for (let i = 0; i < 2; i += 1) {
-//   const dish = await axios(BASE_URL);
-//   console.log(dish);
-//   arr.push(dish);
-//     }
-//     console.log(33333);
-//     const promiseArr = await Promise.all(arr).then(response => {
-//       // return response;
-//       console.log(response);
-//     });
-//     console.log(promiseArr);
-//     return promiseArr;
-//   } catch (error) {
-//     throw new Error(error);
-//   }
-// }
-// console.log(getRandomDish());
-// function createRandomMarkup(arr, data) {
-//   return arr.map(item => {
-//     const { strMeal, strMealThumb } = item.data.meals[0];
-//     `<li>
-//     <img src=${strMealThumb}/>
-//   <p>${strMeal}</p>
-//   </li>`;
-//   });
-// }
-
-// async function createAndRenderRandomMarkup(data) {
-//   try {
-//     const arr = await getRandomDish();
-//     const markup = createRandomMarkup(arr, data);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+async function createAndRenderRandomMarkup() {
+  try {
+    const arr = await getRandomDish();
+    const markup = createRandomMarkup(arr);
+    renderMarkup(refs.viewMenuList, markup);
+  } catch (error) {
+    console.log(error);
+  }
+}
